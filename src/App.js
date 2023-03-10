20f5488c239e53605a58061e2b1e7bdc1669b1e4
tacarr0925 <tacarr@gmail.com>
2023-03-09 22:06:38 -0600

    props

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import './App.css';
 import Employee from './components/Employee';
 
 function App() {
-  console.log('we are about to list the employees')
   const showEmployees = true;
 
   return (
     <div className="App">
-      {console.log('inside the return')}
       {showEmployees ? (
         <>
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
+          <Employee name="Caleb" role="intern"/>
+          <Employee name="Abby" />
+          <Employee name="John" />
         </>
         ) : (
         <p>you cannot see the employees</p>
