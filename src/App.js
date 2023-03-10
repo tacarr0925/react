0227940c4155deda91efde2feed624781df5c790
tacarr0925 <tacarr@gmail.com>
2023-03-09 21:57:47 -0600

    first component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+  console.log('we are about to list the employees')
+  const showEmployees = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log('inside the return')}
+      {showEmployees ? (
+        <>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </>
+        ) : (
+        <p>you cannot see the employees</p>
+      )}
     </div>
   );
 }
