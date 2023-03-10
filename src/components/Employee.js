49ff799384abc61d2fd343fdccee25ceb358610b
tacarr0925 <tacarr@gmail.com>
2023-03-10 07:13:20 -0600

    hooks

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,10 +3,6 @@ function Employee(props) {
     <>
       <h3>Employee {props.name}</h3>
       <p>{props.role ? props.role : "No role"}</p>
-      {props.role ?
-        <p>{props.role}</p> :
-        <p>No role</p>
-      }
     </>
   )
 }
