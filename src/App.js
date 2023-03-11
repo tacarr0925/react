142b2bf251301ff67274c7b31cabc905ac71f841
tacarr0925 <tacarr@gmail.com>
2023-03-10 21:33:42 -0600

    Real update parnet component state in child

--- src/App.js
+++ src/App.js
@@ -8,37 +8,55 @@ function App() {
   const [employees, setEmployees] = useState (
     [
       {
+        id: 1,
         name: "Caleb",
         role: "Developer",
         img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
       },
       {
+        id: 2,
         name: "Sal",
         role: "Manager",
         img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg"
       },
       {
+        id: 3,
         name: "John",
         role: "Director of Eng.",
         img: "https://images.pexels.com/photos/2095582/pexels-photo-2095582.jpeg"
       },
       {
+        id: 4,
         name: "Melanie",
         role: "Software Engineer",
         img: "https://images.pexels.com/photos/3760583/pexels-photo-3760583.jpeg"
       },
       {
+        id: 5,
         name: "Corey",
         role: "The Devops Guy",
         img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
       },
       {
+        id: 6,
         name: "Jake",
         role: "Senior",
         img: "https://images.pexels.com/photos/2225298/pexels-photo-2225298.jpeg"
       }
     ]
   );
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id === employee.id){
+        return {...employee, name: newName, role: newRole}
+      }
+
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
   const showEmployees = true;
 
   return (
@@ -47,15 +65,20 @@ function App() {
         <>
           <input type='text' 
             onChange={(e) => {
-              console.log(e.target.value)
               setRole(e.target.value);
           }}
           />
           <div className="flex flex-wrap justify-center">
             {employees.map((employee) => {
-              console.log();
               return (
-                <Employee key={uuidv4()} name={employee.name} role={employee.role} img={employee.img} />
+                <Employee
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  updateEmployee={updateEmployee}
+                />
               );
             })}
           </div>
--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -25,7 +25,12 @@ function EditEmployee(props) {
           <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <form id='editmodal' className="w-full max-w-sm">
+        <form onSubmit={(e) => {
+            handleClose();
+            e.preventDefault();
+            props.updateEmployee(props.id, name, role);
+        }}
+            id='editmodal' className="w-full max-w-sm">
           <div className="md:flex md:items-center mb-6">
             <div className="md:w-1/3">
               <label
@@ -51,7 +56,7 @@ function EditEmployee(props) {
             <div className="md:w-1/3">
               <label
                 className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                for="Role"
+                for="role"
               >
                 Role
               </label>
