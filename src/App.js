c42af0c05268554deb3895bd55b6f5e5bf854644
tacarr0925 <tacarr@gmail.com>
2023-03-10 22:40:26 -0600

    Pass a component to props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Employee from './components/Employee';
 import { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
 import AddEmployee from './components/AddEmployee';
+import EditEmployee from './components/EditEmployee';
 
 function App() {
   const [role, setRole] = useState('dev');
@@ -81,6 +82,14 @@ function App() {
           />
           <div className="flex flex-wrap justify-center">
             {employees.map((employee) => {
+              const editEmployee = (
+                <EditEmployee
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  updateEmployee={updateEmployee}
+                />
+              );
               return (
                 <Employee
                   key={employee.id}
@@ -88,7 +97,7 @@ function App() {
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
-                  updateEmployee={updateEmployee}
+                  editEmployee={editEmployee}
                 />
               );
             })}
--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -16,7 +16,7 @@ function EditEmployee(props) {
         onClick={handleShow}
         className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
       >
-        Edit
+        Update
       </button>
 
       <Modal
