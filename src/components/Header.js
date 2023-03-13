487e43da6f3017a2b544bdc6d03e8dcb194d71cf
tacarr0925 <tacarr@gmail.com>
2023-03-12 22:12:48 -0500

    Page Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { NavLink } from 'react-router-dom'
 
 const navigation = [
-  { name: 'Employees', href: '/Employees', current: true },
-  { name: 'Customers', href: '/Customers', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Employees', href: '/Employees' },
+  { name: 'Customers', href: '/Customers' },
+  { name: 'Projects', href: '/other' },
+  { name: 'Calendar', href: '/other2' },
 ]
 
 function classNames(...classes) {
@@ -35,17 +36,20 @@ export default function Header(props) {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <NavLink
                         key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'no-underline bg-gray-900 text-white' : ' no-underline text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
+                        to={item.href}
+                        /*className={classNames(
+                          item.current ? ' : ' ',
+                          ''
+                        )}*/
+                        className={({isActive}) => {
+                          return 'rounded-md px-3 py-2 text-sm font-medium ' +
+                          (!isActive ? ' no-underline text-gray-300 hover:bg-gray-700 hover:text-white' : 'no-underline bg-gray-900 text-white')
+                        }}
                       >
                         {item.name}
-                      </a>
+                      </NavLink>
                     ))}
                   </div>
                 </div>
